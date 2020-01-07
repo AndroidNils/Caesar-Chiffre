bc1e1c79c28961e9e242648247f6edfd22b40fe3
Nils Reichardt <stringnils@gmail.com>
2020-01-07 23:09:31 +0100

    Code gekürzt (Scanner entfernt)

--- CaesarChiffre.java
+++ CaesarChiffre.java
@@ -2,23 +2,18 @@ import java.util.Scanner;
 
 public class CaesarChiffre
 {
-    public void start(boolean encrypting) {
-        System.out.println("Bitte gib den Text an, der verschlüsselt, bzw. entschlüsselt werden soll. Zulässig sind nur lateinische Buchstaben.");
-        final String enteredText = new Scanner(System.in).next().toUpperCase().trim();
-
-        System.out.println("Bitte gib die Verschiebung an. Zulässig sind nur Zahlen zwischen 0 & 26.");
-        final int shifting = new Scanner(System.in).nextInt();
-
+    public String start(String text, int key, boolean wirdVerschluesselt) {
+        text = text.toUpperCase().trim();
+        
         // Verschlüsseln / Entschlüsseln
         String result = "";
-        for(int i = 0; i < enteredText.length(); i++) {
-            result += Character.toString((char)(shift(((int) enteredText.charAt(i)) - 65, shifting, encrypting) + 65));
-        }
+        for(int i = 0; i < text.length(); i++)
+            result += (char)(verschieb((text.charAt(i)) - 65, key, wirdVerschluesselt) + 65);
         
-        System.out.println("inputText: " + enteredText + " und shift: " + shifting + " result: " + result);
+        return result;
     }
     
-    private int shift(int letter, int shift, boolean encrypting) {
-        return encrypting ? (letter + shift) % 26 : (letter - (shift - 26)) % 26;
+    private int verschieb(int letter, int shift, boolean wirdVerschluesselt) {
+        return wirdVerschluesselt ? (letter + shift) % 26 : (letter - (shift - 26)) % 26;
     }
 }
