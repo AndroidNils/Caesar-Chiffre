d0442105bc8d064fefa61a77c16a29f6491dd7b3
Nils Reichardt <stringnils@gmail.com>
2020-01-07 23:13:56 +0100

    Refactoring

--- CaesarChiffre.java
+++ CaesarChiffre.java
@@ -5,7 +5,6 @@ public class CaesarChiffre
     public String start(String text, int key, boolean wirdVerschluesselt) {
         text = text.toUpperCase().trim();
         
-        // Verschlüsseln / Entschlüsseln
         String result = "";
         for(int i = 0; i < text.length(); i++)
             result += (char)(verschieb((text.charAt(i)) - 65, key, wirdVerschluesselt) + 65);
@@ -13,7 +12,7 @@ public class CaesarChiffre
         return result;
     }
     
-    private int verschieb(int letter, int shift, boolean wirdVerschluesselt) {
-        return wirdVerschluesselt ? (letter + shift) % 26 : (letter - (shift - 26)) % 26;
+    private int verschieb(int buchstabe, int key, boolean wirdVerschluesselt) {
+        return wirdVerschluesselt ? (buchstabe + key) % 26 : (buchstabe - (key - 26)) % 26;
     }
 }
